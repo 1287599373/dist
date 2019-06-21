5181264f33ec3b5b8dff22c590d478e4d114b9d7
wenshanxiang <example@for.com>
2019-06-21 15:25:50 +0800

    test Signed-off-by: wenshanxiang <example@for.com>

--- dist/common/common.js
+++ dist/common/common.js
@@ -1,2 +1,3 @@
-var baseUrl = "http://localhost:8080/transport";
+//var baseUrl = "http://localhost:8080/transport";
+var baseUrl = "http://127.0.0.1:8088/api";
 var attachUrl = "http://pic37.nipic.com/20140113/";
